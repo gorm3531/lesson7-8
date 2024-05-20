707a7f85e459b7a4a43fab44eb4337ad22419f39
gorm3531 <mcgermafrodit@gmail.com>
2024-05-20 23:52:08 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+immutable_var = 1, 'Help', 3.0
+print(immutable_var) #2
+#immutable_var [0] = 2
+#print(immutable_var)
+# 3. Кортеж является неизменяемым, упорядочным объектом
+mutable_list = ['food', 1, 2.0]
+print(mutable_list)
+mutable_list [0] = "peach"
+mutable_list [1] = 200
+print(mutable_list) #4
+
+
+
